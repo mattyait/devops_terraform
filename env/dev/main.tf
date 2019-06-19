567db6eb4519a19dc12971ac96e5a933176aeae6
mattyait <mattyait@gmail.com>
2019-06-19 07:14:01 +0000

    added nat gateway module

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -12,12 +12,20 @@ module "vpc" {
   environment    = "${var.environment}"
 }
 
+
 module "public_subnet_1a" {
   source            = "../../modules/aws/subnet"
   vpc_id            = "${module.vpc.vpc_id_out}"
   cidr_block        = "${var.public_subnet_1a_cidr_block}"
   availability_zone = "ap-southeast-1a"
   environment       = "${var.environment}"
+  type              = "public"
+}
+
+
+module "nat_gateway_1a" {
+  source    = "../../modules/aws/nat_gateway"
+  subnet_id = "${module.public_subnet_1a.subnet_id_out}"
 }
 
 module "public_subnet_1b" {
@@ -26,6 +34,12 @@ module "public_subnet_1b" {
   cidr_block        = "${var.public_subnet_1b_cidr_block}"
   availability_zone = "ap-southeast-1b"
   environment       = "${var.environment}"
+  type              = "public"
+}
+
+module "nat_gateway_1b" {
+  source    = "../../modules/aws/nat_gateway"
+  subnet_id = "${module.public_subnet_1b.subnet_id_out}"
 }
 
 module "private_subnet_1a" {
@@ -34,6 +48,7 @@ module "private_subnet_1a" {
   cidr_block        = "${var.private_subnet_1a_cidr_block}"
   availability_zone = "ap-southeast-1a"
   environment       = "${var.environment}"
+  type              = "private"
 }
 
 module "private_subnet_1b" {
@@ -42,4 +57,6 @@ module "private_subnet_1b" {
   cidr_block        = "${var.private_subnet_1b_cidr_block}"
   availability_zone = "ap-southeast-1b"
   environment       = "${var.environment}"
+  type              = "private"
 }
+
--- modules/aws/subnet/variables.tf
+++ modules/aws/subnet/variables.tf
@@ -2,3 +2,4 @@ variable vpc_id                  {}
 variable cidr_block              {}
 variable availability_zone       {}
 variable environment             {}
+variable type                    {}
