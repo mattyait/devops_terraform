7731ef2fcc0def4c74cae46b41ec87bd07169d16
mattyait <mattyait@gmail.com>
2019-06-20 05:34:32 +0000

    Using variables for eks_cluster

--- env/dev/eks_cluster.tf
+++ env/dev/eks_cluster.tf
@@ -15,12 +15,22 @@ module "eks_cluster" {
 
   worker_groups = [
     {
-      instance_type = "m1.medium"
-      asg_max_size  = 5
+      name                 = "eks_workers"
+      instance_type        = "${var.instance_type}"
+      asg_min_size         = "${var.asg_min_size}"
+      asg_desired_capacity = "${var.asg_desired_capacity}"
+      asg_max_size         = "${var.asg_max_size}"
+      root_volume_size     = "${var.root_volume_size}"
+      root_volume_type     = "${var.root_volume_type}"
+      ami_id               = "${var.ami_id}"
+      ebs_optimized        = false
+      key_name             = "${var.key_name}"
+      enable_monitoring    = false
     }
   ]
 
   tags = {
     Cluster = "k8s"
+    Name = "${var.environment}_eks_workers"
   }
 }
